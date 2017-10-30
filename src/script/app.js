a6707ee8ebfbd1677495fef345e1f0e5bcb121df
wangdayewjf <948354159@qq.com>
2017-10-30 22:21:48 +0800

    react 启动成功

--- src/script/app.js
+++ src/script/app.js
@@ -1,42 +1,43 @@
 document.write( require("./js/runoob1.js"));
 
 //es6语法
-/*import App  from './components/component1';
-
+import App  from './components/component1';
+import React from 'react';
+import ReactDOM from 'react-dom';
 const element=<App/>;
 ReactDOM.render(element, document.getElementById('root'));
-*/
+
 
 //es5语法
-var React = require("react");
-var ReactDOM = require('react-dom');
-var App = require('./components/component1');
-var Welcome = React.createClass({
-    propTypes: {//定义传入props中的属性各种类型
-        initialValue: React.PropTypes.string
-    },
-    defaultProps: { //组件默认的props对象
-        initialValue: ''
-    },
-    // 设置 initial state
-    getInitialState: function() {//组件相关的状态对象
-        return {
-            text: this.props.initialValue || 'placeholder'
-        };
-    },
-    handleChange: function(event) {
-        this.setState({ //this represents react component instance
-            text: event.target.value
-        });
-    },
-    render: function() {
-        return (
-            <div>
-                Type something:
-                <input onChange={this.handleChange} value={this.state.text} />
-            </div>
-        );
-    }
-});
+// var React = require("react");
+// var ReactDOM = require('react-dom');
+// var App = require('./components/component1');
+// var Welcome = React.createClass({
+//     propTypes: {//定义传入props中的属性各种类型
+//         initialValue: React.PropTypes.string
+//     },
+//     defaultProps: { //组件默认的props对象
+//         initialValue: ''
+//     },
+//     // 设置 initial state
+//     getInitialState: function() {//组件相关的状态对象
+//         return {
+//             text: this.props.initialValue || 'placeholder'
+//         };
+//     },
+//     handleChange: function(event) {
+//         this.setState({ //this represents react component instance
+//             text: event.target.value
+//         });
+//     },
+//     render: function() {
+//         return (
+//             <div>
+//                 Type something:
+//                 <input onChange={this.handleChange} value={this.state.text} />
+//             </div>
+//         );
+//     }
+// });
 
-ReactDOM.render(<Welcome name="Sara"/>, document.getElementById('root'));
+//ReactDOM.render(<Welcome name="Sara"/>, document.getElementById('root'));
