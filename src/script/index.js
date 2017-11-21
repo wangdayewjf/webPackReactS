a7b62162755f8f159597e924b35fd95e09dedcc4
wangjianfeng <wang.jianfeng@yahoo.com>
2017-11-21 18:30:28 +0800

    xxx

--- src/script/index.js
+++ src/script/index.js
@@ -5,25 +5,79 @@ import { Provider } from 'react-redux'
 import '../css/index.css'
 import CommentApp from './containers/CommentApp'
 import reducer from './reducers/comments';
-import {
+/*import {
   BrowserRouter as Router,
   Route,
-  Link
-} from 'react-router-dom';
+  Link,
+  hashHistory
+} from 'react-router-dom';*/
 
+import { Router, Route, Link } from 'react-router'
 
 const store = createStore(reducer)//初始化store
 
 class Index extends Component {
   render () {
     return (
-      <div>
-        
-        <CommentApp />
-      </div>
+       <Router>
+        <Route path="/" component={CommentApp}>
+          <Route path="about" component={About} />
+          <Route path="inbox" component={Inbox}>
+            <Route path="messages/:id" component={Message} />
+          </Route>
+        </Route>
+      </Router>
     )
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+//通过match传递参数可能是新特性，阮一峰的课程里面没有提到。
+//
 
 ReactDOM.render(
   <Provider store={store}>
