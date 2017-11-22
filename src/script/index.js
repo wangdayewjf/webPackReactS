0ce2a606a81800cc739e36d4f1e6a3f85f097cc0
wangjianfeng <wang.jianfeng@yahoo.com>
2017-11-22 14:41:45 +0800

    add

--- src/script/index.js
+++ src/script/index.js
@@ -5,17 +5,15 @@ import { Provider } from 'react-redux'
 import '../css/index.css'
 import CommentApp from './containers/CommentApp'
 import reducer from './reducers/comments';
-/*import {
+
+import {
   BrowserRouter as Router,
   Route,
-  Link,
-  hashHistory
-} from 'react-router-dom';*/
-
-import { Router, Route, Link } from 'react-router'
+  Link
+} from 'react-router-dom'
 
 const store = createStore(reducer)//初始化store
-
+/*
 class Index extends Component {
   render () {
     return (
@@ -29,22 +27,38 @@ class Index extends Component {
       </Router>
     )
   }
-}
+}*/
 
-const Home = () => (
+const Index = ()=>(
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </div>
+    </Router>
+  );
+
+//笔记：
+//1，2.x到4.x版本的变化就是，原先默认会自动匹配router下的第一个，而，现在只匹配
+
+const Home = ()=>(
   <div>
     <h2>Home</h2>
   </div>
-)
+ );
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
 
-const Topics = ({ match }) => (
-  <div>
+const Topics = ({match})=>(
+    <div>
     <h2>Topics</h2>
     <ul>
       <li>
@@ -68,16 +82,25 @@ const Topics = ({ match }) => (
     <Route exact path={match.url} render={() => (
       <h3>Please select a topic.</h3>
     )}/>
-  </div>
-)
+  </div>  
 
-const Topic = ({ match }) => (
+  );
+
+const Topic = ({match})=>(
+<div>
+  <h3>{match.params.topicId}</h3>
+</div>
+  );
+
+
+
+//问：match这款参数口有点类似于，一般模块的prop?
+
+const About = ()=>(
   <div>
-    <h3>{match.params.topicId}</h3>
+    <h2>About</h2>
   </div>
-)
-//通过match传递参数可能是新特性，阮一峰的课程里面没有提到。
-//
+ );
 
 ReactDOM.render(
   <Provider store={store}>
@@ -85,5 +108,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
+
 
 
